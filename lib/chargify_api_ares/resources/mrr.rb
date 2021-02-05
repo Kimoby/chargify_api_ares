ac5c21b9f83ad1a492f4865a4649c9ac19a62337
Ismaël Meskin <ismael.m@kimoby.com>
2021-02-04 22:27:23 -0500

    Update mrr.rb

--- lib/chargify_api_ares/resources/mrr.rb
+++ lib/chargify_api_ares/resources/mrr.rb
@@ -3,9 +3,5 @@ module Chargify
     include ResponseHelper
 
     self.prefix = '/mrr?subscription_id=:subscription_id/'
-
-    def self.price_points(subscription_id)
-      find(:first, :params => {:subscription_id => subscription_id})
-    end
   end
 end
