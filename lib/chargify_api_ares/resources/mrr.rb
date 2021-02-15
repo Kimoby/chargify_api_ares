b57c2f5a5dac0999b18e96c1f50ed5166616a7a5
Ismaël Meskin <ismael.m@kimoby.com>
2021-02-15 15:20:27 -0500

    Update mrr.rb

--- lib/chargify_api_ares/resources/mrr.rb
+++ lib/chargify_api_ares/resources/mrr.rb
@@ -5,7 +5,7 @@ module Chargify
     self.prefix = '/mrr'
 
     def self.find_by_subscription_id(subscription_id)
-			find(:one, { params: { subscription_id: subscription_id } })
+			find(:one, from: :lookup, params: { subscription_id: subscription_id })
 		end
   end
 end
