dfca130238b49070c817f0e79ea6f4b7da4ff22b
Ismaël Meskin <ismael.m@kimoby.com>
2021-02-15 18:49:24 -0500

    Update mrr.rb

--- lib/chargify_api_ares/resources/mrr.rb
+++ lib/chargify_api_ares/resources/mrr.rb
@@ -2,8 +2,6 @@ module Chargify
   class Mrr < Base
     include ResponseHelper
 
-    self.prefix = '/mrr'
-
     def self.find_by_subscription_id(subscription_id)
 			find(:one, params: { subscription_id: subscription_id })
 		end
